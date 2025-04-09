2be64dc22970c08fa2a9312bbc624a1aa2328edc
24LP1557 24LP1557 <24LP1557@G305-CC0109.local>
2025-04-09 17:37:02 +0800

    k

--- src/app/_components/StepOne.jsx
+++ src/app/_components/StepOne.jsx
@@ -1,68 +1,124 @@
 "use client"
 import React from 'react'
-export const StepOne = ({
-    onChange,
-    onClickNext,
-    inputValue,
-    errors,
-    borderColor1,
-    borderColor2,
-    borderColor3, 
-}) => {   
-    return( 
-        <div className="bg-[#FFFFFF] text-black rounded-lg w-[480px] min-h-[655px] flex flex-col items-start">
+
+export const StepOne = ({ setCurrentStep, onChange, form }) => {
+    return (
+        <div>
             <div className="p-[32px]">
                 <div className="w-[416px] h-[129px] flex flex-col justify-center">
                     <img className="w-[60px] h-[60px]" src="/logo.svg" alt="mainLogo"></img>
                     <p className="font-semibold text-lg">Join Us! 😎</p> 
                     <p className="font-lg text-[#8E8E8E]">Please provide all current information accurately.</p>
                 </div>
-                <div className="w-416px] flex flex-col justify-between">
-                    <p className="text-sm font-semibold text-[#8E8E8E] mt-[10px]">First Name *</p> 
-                    <input 
-                        className={`text-black w-[416px] h-[44px] rounded-lg border-[#0CA5E9] ${borderColor1} border border-solid flex gap-3 bg-white pl-2 mb-4`}
-                        id="firstName" 
-                        value={inputValue.firstName}  
-                        onChange={onChange}  
-                        placeholder={'Enter your firstname...'} 
-                        type='text'
-                    /> 
-                    <p className="text-red-700 text-[14px]">{errors.firstName}</p>
+                <div>
+                    <form action={""} method="">
+                        <label htmlFor="firstName">FirstName</label>
+                        <input
+                            onChange={onChange}
+                            id="firstName"  
+                            placeholder='Enter your firstname...' 
+                            type="text" 
+                            className="border" 
+                            value={form.firstName}
+                        />
 
-                    <p className="text-sm font-semibold text-[#8E8E8E] mt-[10px]">Last Name *</p>
-                    <input 
-                        className={`text-black w-[416px] h-[44px] rounded-lg border-[#0CA5E9] ${borderColor2} border border-solid flex gap-3 bg-white pl-2 mb-4`}
-                        id="lastName" 
-                        value={inputValue.lastName} 
-                        onChange={onChange} 
-                        placeholder={'Enter your lastname...'} 
-                        type='text' 
-                    />
-                    <p className="text-red-700 text-[14px]">{errors.lastName}</p>
+                        <label htmlFor="firstName">LastName</label>
+                        <input
+                            onChange={onChange}
+                            id="lastName"  
+                            placeholder='Enter your lastname...' 
+                            type="text" 
+                            className="border" 
+                            value={form.lastName}
+                        />
 
-                    <p className="text-sm font-semibold text-[#8E8E8E] mt-[10px]">Username *</p> 
-                    <input
-                        className={`text-black w-[416px] h-[44px] rounded-lg border-[#0CA5E9] ${borderColor3} border border-solid flex gap-3 bg-white pl-2 mb-4`}
-                        id="userName" 
-                        value={inputValue.userName}  
-                        onChange={onChange} 
-                        placeholder={'Enter your username...'}
-                        type='text'
-                    />  
-                    <p className="text-red-700 text-[14px]">{errors.userName}</p>
-                </div> 
-                <div className="w-[416px] h-[44px] flex flex-col mt-[140px]">
-                    <button
-                        className="flex justify-center items-center bg-[#D6D8DB] text-[#334155] w-[416px] h-[44px] border border-[#D6D8DB] rounded-lg"
-                         
-                        onClick={onClickNext}>
-                        {"Continue 1/3 >"}
-                    </button> 
+                        <label htmlFor="firstName">UserName</label>
+                        <input
+                            onChange={onChange}
+                            id="userName"  
+                            placeholder='Enter your username...' 
+                            type="text" 
+                            className="border" 
+                            value={form.userName}
+                        />
+                    </form>
                 </div>
+                <button onClick={() => setCurrentStep(2)}>Next</button>
             </div>
-        </div> 
+        </div>
     )
 }
 
-//  propsoor function useState ali n ch ywj bolhimu? yess..
-//  gadnaas hergelj bga ymaa propsoor oruulj irsen tgd deer butsaagaad ingej ashiglasan shu gd export hijigamu? yess..
+
+
+
+
+
+// "use client"
+// import React from 'react'
+// export const StepOne = ({
+//     onChange,
+//     onClickNext,
+//     inputValue,
+//     errors,
+//     borderColor1,
+//     borderColor2,
+//     borderColor3, 
+// }) => {   
+//     return( 
+//         <div className="bg-[#FFFFFF] text-black rounded-lg w-[480px] min-h-[655px] flex flex-col items-start">
+//             <div className="p-[32px]">
+                // <div className="w-[416px] h-[129px] flex flex-col justify-center">
+                //     <img className="w-[60px] h-[60px]" src="/logo.svg" alt="mainLogo"></img>
+                //     <p className="font-semibold text-lg">Join Us! 😎</p> 
+                //     <p className="font-lg text-[#8E8E8E]">Please provide all current information accurately.</p>
+                // </div>
+//                 <div className="w-416px] flex flex-col justify-between">
+//                     <p className="text-sm font-semibold text-[#8E8E8E] mt-[10px]">First Name *</p> 
+//                     <input 
+//                         className={`text-black w-[416px] h-[44px] rounded-lg border-[#0CA5E9] ${borderColor1} border border-solid flex gap-3 bg-white pl-2 mb-4`}
+//                         id="firstName" 
+//                         value={inputValue.firstName}  
+//                         onChange={onChange}  
+//                         placeholder={'Enter your firstname...'} 
+//                         type='text'
+//                     /> 
+//                     <p className="text-red-700 text-[14px]">{errors.firstName}</p>
+
+//                     <p className="text-sm font-semibold text-[#8E8E8E] mt-[10px]">Last Name *</p>
+//                     <input 
+//                         className={`text-black w-[416px] h-[44px] rounded-lg border-[#0CA5E9] ${borderColor2} border border-solid flex gap-3 bg-white pl-2 mb-4`}
+//                         id="lastName" 
+//                         value={inputValue.lastName} 
+//                         onChange={onChange} 
+//                         placeholder={'Enter your lastname...'} 
+//                         type='text' 
+//                     />
+//                     <p className="text-red-700 text-[14px]">{errors.lastName}</p>
+
+//                     <p className="text-sm font-semibold text-[#8E8E8E] mt-[10px]">Username *</p> 
+//                     <input
+//                         className={`text-black w-[416px] h-[44px] rounded-lg border-[#0CA5E9] ${borderColor3} border border-solid flex gap-3 bg-white pl-2 mb-4`}
+//                         id="userName" 
+//                         value={inputValue.userName}  
+//                         onChange={onChange} 
+//                         placeholder={'Enter your username...'}
+//                         type='text'
+//                     />  
+//                     <p className="text-red-700 text-[14px]">{errors.userName}</p>
+//                 </div> 
+//                 <div className="w-[416px] h-[44px] flex flex-col mt-[140px]">
+//                     <button
+//                         className="flex justify-center items-center bg-[#D6D8DB] text-[#334155] w-[416px] h-[44px] border border-[#D6D8DB] rounded-lg"
+                         
+//                         onClick={onClickNext}>
+//                         {"Continue 1/3 >"}
+//                     </button> 
+//                 </div>
+//             </div>
+//         </div> 
+//     )
+// }
+
+// //  propsoor function useState ali n ch ywj bolhimu? yess..
+// //  gadnaas hergelj bga ymaa propsoor oruulj irsen tgd deer butsaagaad ingej ashiglasan shu gd export hijigamu? yess..
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,18 @@
-'use client'; 
-import Image from "next/image";
+"use client";
+import React from 'react'
 import { useState } from 'react';
-import { useEffect } from 'react';
 import { StepOne } from '@/app/_components/StepOne';
 import { StepTwo } from '@/app/_components/StepTwo';
-import { StepThree } from '@/app/_components/StepThree';
-import { StepFour } from '@/app/_components/StepFour';
-import { validateStepOne, validateStepTwo } from "@/helpers/validation";
 
-export default function Home() {
+const FormBody = ({ currentStep, setCurrentStep, onChange, form }) => {
+  if (currentStep === 1) {
+    return <StepOne setCurrentStep={setCurrentStep} onChange={onChange} form={form} />
+  }
+}
+
+export default function Home () {
   const [currentStep, setCurrentStep] = useState(1);
-  const [inputValue, setInputValue] = useState({ 
+  const [form, setForm] = useState({
     firstName: '',
     lastName: '',
     userName:'',
@@ -21,140 +23,184 @@ export default function Home() {
     dateOfBirth:'',
     profileImage:'',
   });
-  const [error, setError] = useState({
-    firstName: '',
-    lastName: '',
-    userName:'',
-    email:'',
-    phoneNumber:'',
-    password:'',
-    confirmPassword:'',
-    dateOfBirth:'',
-    profileImage:'',
-  });
-  const [borderColor1, setBorderColor1] = useState("focus:border-[#CBD5E1]");
-  const [borderColor2, setBorderColor2] = useState("focus:border-[#CBD5E1]");
-  const [borderColor3, setBorderColor3] = useState("focus:border-[#CBD5E1]");
-  const [borderColor4, setBorderColor4] = useState("focus:border-[#CBD5E1]");
-  const [borderColor5, setBorderColor5] = useState("focus:border-[#CBD5E1]");
-  const [borderColor6, setBorderColor6] = useState("focus:border-[#CBD5E1]");
-  const [borderColor7, setBorderColor7] = useState("focus:border-[#CBD5E1]");
-  const [firstbuttonColor, setfirstButtonColor] = useState("");
-  const [secondButtonColor, setSecondButtonColor] = useState("");
-  
-  useEffect(()=>{
-      if (error.firstName != '') {
-        setBorderColor1("border:[#E14942]")
-    }
-  }, [error]);
 
-  const inputChange = (e)=> {
-    const inputId = e.target.id;
-      console.log(inputId)
-    const newValues = {...inputValue, [inputId]: e.target.value}
-      console.log(newValues)
-    setInputValue(newValues);
-    
-    const {isValid, newErrors} = validateStepOne(newValues);
-      console.log(newErrors,'errrororororoo')
-    setError(newErrors)
-    if(newErrors.firstName !== ""){
-      setBorderColor1("border:[#E14942]")
-    }
-    if(newErrors.lastName !== ""){
-      setBorderColor2("border:[#E14942]")
-    } 
-    if(newErrors.userName !== ""){
-      setBorderColor3("border:[#E14942]")
-    } 
-  };
-
-  const inputChangeTwo = (e)=> {
-    const inputId = e.target.id;
-      console.log(inputId)
-    const newValues = {...inputValue, [inputId]: e.target.value}
-      console.log(newValues)
-    setInputValue(newValues);
-      console.log(newValues)
-
-    const {isValidTwo, newErrorsTwo} = validateStepTwo(newValues);
-      console.log(newErrorsTwo,'errrororororoo')    
-    setError(newErrorsTwo)
-    if(newErrorsTwo.email !== ""){
-      setBorderColor4("border:[#E14942]")
-    }
-    if(newErrorsTwo.phoneNumber !== ""){
-      setBorderColor5("border:[#E14942]")
-    } 
-    if(newErrorsTwo.password !== ""){
-      setBorderColor6("border:[#E14942]")
-    } 
-    if(newErrorsTwo.confirmPassword !== ""){
-      setBorderColor7("border:[#E14942]")
-    } 
-  };
-  
-  const nextCard = ()=>{
-    setCurrentStep(currentStep+1);
-  };
-
-  const backCard = ()=>{
-    setCurrentStep(currentStep-1);
-  };
-
-  const inputChangeThree = (e)=> {
-    const inputId = e.target.id;
-      console.log(inputId)
-    const newValues = {...inputValue, [inputId]: e.target.value}
-      console.log(newValues)
-    setInputValue(newValues);
-    const {isValidTwo, newErrorsTwo} = validateStepTwo(newValues);
-    console.log(newErrorsTwo,'errrororororoo')    
-  setError(newErrorsTwo)
-};
+  const onChange = () => {};
 
   return (
-    <>
-      <div className="bg-stone-300 w-full h-screen flex justify-center items-center">
-        {currentStep === 1 && (
-          <StepOne
-          inputValue={inputValue}
-          onChange={inputChange} //end onChange gej nerlcheed StepOne dree onChange gj uguh l heregtei bsn !!!zgeerl StepOne neree export hiisen! 
-          onClickNext={nextCard}
-          errors={error}
-          borderColor1={borderColor1} 
-          borderColor2={borderColor2}
-          borderColor3={borderColor3} 
-          />
-        )} 
-        {currentStep === 2 && (
-          <StepTwo
-          inputValue={inputValue}
-          onChange={inputChangeTwo}
-          onClickBack={backCard}
-          onClickNext={nextCard}
-          errors={error}
-          borderColor4={borderColor4} 
-          borderColor5={borderColor5}
-          borderColor6={borderColor6}
-          borderColor7={borderColor7}
-          />
-        )}
-         {currentStep === 3 && (
-          <StepThree
-          inputValue={inputValue}
-          onChange={inputChangeThree}
-          onClickBack={backCard}
-          onClickNext={nextCard}
-          errors={error}
-          />
-        )}
-
-         {currentStep === 4 && (
-          <StepFour/>
-        )}
-      </div>
-    </>
-  )
+    <div>
+      <FormBody 
+      currentStep={currentStep} 
+      setCurrentStep={setCurrentStep}
+      onChange={onChange}
+      form={form}
+      />
+    </div>
+  );
 }
+
+
+
+
+
+
+
+// 'use client'; 
+// import Image from "next/image";
+// import { useState } from 'react';
+// import { useEffect } from 'react';
+// import { StepOne } from '@/app/_components/StepOne';
+// import { StepTwo } from '@/app/_components/StepTwo';
+// import { StepThree } from '@/app/_components/StepThree';
+// import { StepFour } from '@/app/_components/StepFour';
+// import { validateStepOne, validateStepTwo } from "@/helpers/validation";
+
+// export default function Home() {
+//   const [currentStep, setCurrentStep] = useState(1);
+//   const [inputValue, setInputValue] = useState({ 
+//     firstName: '',
+//     lastName: '',
+//     userName:'',
+//     email:'',
+//     phoneNumber:'',
+//     password:'',
+//     confirmPassword:'',
+//     dateOfBirth:'',
+//     profileImage:'',a
+//   });
+//   const [error, setError] = useState({
+//     firstName: '',
+//     lastName: '',
+//     userName:'',
+//     email:'',
+//     phoneNumber:'',
+//     password:'',
+//     confirmPassword:'',
+//     dateOfBirth:'',
+//     profileImage:'',
+//   });
+//   const [borderColor1, setBorderColor1] = useState("focus:border-[#CBD5E1]");
+//   const [borderColor2, setBorderColor2] = useState("focus:border-[#CBD5E1]");
+//   const [borderColor3, setBorderColor3] = useState("focus:border-[#CBD5E1]");
+//   const [borderColor4, setBorderColor4] = useState("focus:border-[#CBD5E1]");
+//   const [borderColor5, setBorderColor5] = useState("focus:border-[#CBD5E1]");
+//   const [borderColor6, setBorderColor6] = useState("focus:border-[#CBD5E1]");
+//   const [borderColor7, setBorderColor7] = useState("focus:border-[#CBD5E1]");
+//   const [firstbuttonColor, setfirstButtonColor] = useState("");
+//   const [secondButtonColor, setSecondButtonColor] = useState("");
+  
+//   useEffect(()=>{
+//       if (error.firstName != '') {
+//         setBorderColor1("border:[#E14942]")
+//     }
+//   }, [error]);
+
+//   const inputChange = (e)=> {
+//     const inputId = e.target.id;
+//       console.log(inputId)
+//     const newValues = {...inputValue, [inputId]: e.target.value}
+//       console.log(newValues)
+//     setInputValue(newValues);
+    
+//     const {isValid, newErrors} = validateStepOne(newValues);
+//       console.log(newErrors,'errrororororoo')
+//     setError(newErrors)
+//     if(newErrors.firstName !== ""){
+//       setBorderColor1("border:[#E14942]")
+//     }
+//     if(newErrors.lastName !== ""){
+//       setBorderColor2("border:[#E14942]")
+//     } 
+//     if(newErrors.userName !== ""){
+//       setBorderColor3("border:[#E14942]")
+//     } 
+//   };
+
+//   const inputChangeTwo = (e)=> {
+//     const inputId = e.target.id;
+//       console.log(inputId)
+//     const newValues = {...inputValue, [inputId]: e.target.value}
+//       console.log(newValues)
+//     setInputValue(newValues);
+//       console.log(newValues)
+
+//     const {isValidTwo, newErrorsTwo} = validateStepTwo(newValues);
+//       console.log(newErrorsTwo,'errrororororoo')    
+//     setError(newErrorsTwo)
+//     if(newErrorsTwo.email !== ""){
+//       setBorderColor4("border:[#E14942]")
+//     }
+//     if(newErrorsTwo.phoneNumber !== ""){
+//       setBorderColor5("border:[#E14942]")
+//     } 
+//     if(newErrorsTwo.password !== ""){
+//       setBorderColor6("border:[#E14942]")
+//     } 
+//     if(newErrorsTwo.confirmPassword !== ""){
+//       setBorderColor7("border:[#E14942]")
+//     } 
+//   };
+  
+//   const nextCard = ()=>{
+//     setCurrentStep(currentStep+1);
+//   };
+
+//   const backCard = ()=>{
+//     setCurrentStep(currentStep-1);
+//   };
+
+//   const inputChangeThree = (e)=> {
+//     const inputId = e.target.id;
+//       console.log(inputId)
+//     const newValues = {...inputValue, [inputId]: e.target.value}
+//       console.log(newValues)
+//     setInputValue(newValues);
+//     const {isValidTwo, newErrorsTwo} = validateStepTwo(newValues);
+//     console.log(newErrorsTwo,'errrororororoo')    
+//   setError(newErrorsTwo)
+// };
+
+//   return (
+//     <>
+//       <div className="bg-stone-300 w-full h-screen flex justify-center items-center">
+//         {currentStep === 1 && (
+//           <StepOne
+//           inputValue={inputValue}
+//           onChange={inputChange} //end onChange gej nerlcheed StepOne dree onChange gj uguh l heregtei bsn !!!zgeerl StepOne neree export hiisen! 
+//           onClickNext={nextCard}
+//           errors={error}
+//           borderColor1={borderColor1} 
+//           borderColor2={borderColor2}
+//           borderColor3={borderColor3} 
+//           />
+//         )} 
+//         {currentStep === 2 && (
+//           <StepTwo
+//           inputValue={inputValue}
+//           onChange={inputChangeTwo}
+//           onClickBack={backCard}
+//           onClickNext={nextCard}
+//           errors={error}
+//           borderColor4={borderColor4} 
+//           borderColor5={borderColor5}
+//           borderColor6={borderColor6}
+//           borderColor7={borderColor7}
+//           />
+//         )}
+//          {currentStep === 3 && (
+//           <StepThree
+//           inputValue={inputValue}
+//           onChange={inputChangeThree}
+//           onClickBack={backCard}
+//           onClickNext={nextCard}
+//           errors={error}
+//           />
+//         )}
+
+//          {currentStep === 4 && (
+//           <StepFour/>
+//         )}
+//       </div>
+//     </>
+//   )
+// }
 
